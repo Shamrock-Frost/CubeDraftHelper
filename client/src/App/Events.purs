3e07414f5ce18332472da952d5f1eb67a250cdfc
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-05-24 14:11:41 -0700

    Began work on pux client

--- client/src/App/Events.purs
+++ client/src/App/Events.purs
@@ -2,13 +2,26 @@ module App.Events where
 
 import App.Routes (Route)
 import App.State (State(..))
+import Control.Monad.Aff.Console (log)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Class (liftEff)
 import Data.Function (($))
+import Data.Functor (($>))
+import Data.Maybe (Maybe(Nothing))
 import Network.HTTP.Affjax (AJAX)
-import Pux (EffModel, noEffects)
+import Pux (EffModel, noEffects, onlyEffects)
 
 data Event = PageView Route
+           | Host
+           | Join
 
-type AppEffects fx = (ajax :: AJAX | fx)
+type AppEffects fx = (console :: CONSOLE | fx)
 
-foldp :: ∀ fx. Event -> State -> EffModel State Event (AppEffects fx)
+foldp :: forall fx. Event -> State -> EffModel State Event (AppEffects fx)
 foldp (PageView route) (State st) = noEffects $ State st { route = route, loaded = true }
+foldp Host             (State st) = { state: State st
+                                    , effects: [log "hosted" $> Nothing]
+                                    }
+foldp Join             (State st) = { state: State st
+                                    , effects: [log "joined" $> Nothing]
+                                    }
--- client/src/App/View/Homepage.purs
+++ client/src/App/View/Homepage.purs
@@ -1,21 +1,22 @@
 module App.View.Homepage where
 
-import App.Events (Event)
+import App.Events (Event(..))
 import App.State (State)
 import Control.Bind (discard)
-import Data.Function (($))
+import Data.Function (($), const)
+import Pux.DOM.Events (onClick)
 import Pux.DOM.HTML (HTML)
 import Text.Smolder.HTML (a, div, h1, h2)
-import Text.Smolder.HTML.Attributes (href, className)
-import Text.Smolder.Markup ((!), text)
+import Text.Smolder.HTML.Attributes (className)
+import Text.Smolder.Markup ((!), (#!), text)
 
 view :: State -> HTML Event
 view s =
   div do
     h1 $ text "Cube Draft"
     a ! className "host"
-      ! href "/api/Host"
+      #! onClick (const Host)
         $ h2 $ text "Host"
     a ! className "join"
-      ! href "/api/Host"
+      #! onClick (const Join)
         $ h2 $ text "Join"
