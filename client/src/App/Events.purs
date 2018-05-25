9a2fa2bce59b5dc0102874e2abf037ff5448ae36
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-05-25 04:13:25 -0700

    Switched server to use Spock and  started front-backend communication

--- client/src/App/Events.purs
+++ client/src/App/Events.purs
@@ -2,26 +2,30 @@ module App.Events where
 
 import App.Routes (Route)
 import App.State (State(..))
+import Control.Monad.Aff (attempt)
 import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Class (liftEff)
 import Data.Function (($))
 import Data.Functor (($>))
 import Data.Maybe (Maybe(Nothing))
-import Network.HTTP.Affjax (AJAX)
+import Network.HTTP.Affjax (AJAX, get)
+import Prelude (bind, discard, (<>), pure)
 import Pux (EffModel, noEffects, onlyEffects)
 
 data Event = PageView Route
            | Host
            | Join
 
-type AppEffects fx = (console :: CONSOLE | fx)
+type AppEffects fx = (ajax :: AJAX, console :: CONSOLE | fx)
 
 foldp :: forall fx. Event -> State -> EffModel State Event (AppEffects fx)
-foldp (PageView route) (State st) = noEffects $ State st { route = route, loaded = true }
-foldp Host             (State st) = { state: State st
-                                    , effects: [log "hosted" $> Nothing]
-                                    }
-foldp Join             (State st) = { state: State st
-                                    , effects: [log "joined" $> Nothing]
-                                    }
+foldp e (State st) = case e of
+  PageView route -> noEffects $ State st { route = route, loaded = true }
+  Host           -> echoServer "http://localhost:8000/host"
+  Join           -> echoServer "http://localhost:8000/join"
+  where echoServer url =  { state: State st
+                          , effects: [do res <- get url
+                                         log ("Server says: " <> res.response)
+                                         pure Nothing]
+                          }
--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
 module Main where
 
-import Lib
+import Web.Spock
+import Web.Spock.Config
+
+import Control.Monad.Trans
+import Data.IORef
+import qualified Data.Text as T
+
+data PlayerStatus = Host | Join deriving (Show, Read)
+
+data Session = EmptySession
+data AppState = NoAction
+              | InLobby PlayerStatus
 
 main :: IO ()
-main = startApp
+main = do
+  ref <- newIORef NoAction
+  spockCfg <- defaultSpockCfg EmptySession PCNoDatabase ref
+  runSpock 8000 (spock spockCfg app)
+
+app :: SpockM () Session (IORef AppState) ()
+app = do get (root <//> static "host") $ handleReq Host
+         get (root <//> static "join") $ handleReq Join
+  where handleReq :: PlayerStatus -> ActionCtxT () (WebStateM () Session (IORef AppState)) ()
+        handleReq x = do
+          ref <- getState
+          state <- liftIO $ readIORef ref
+          case state of
+            NoAction  -> do liftIO $ writeIORef ref (InLobby x)
+                            setHeader "Access-Control-Allow-Origin" "*"
+                            text (T.pack $ "Begin process \"" ++ show x ++ "\"")
+            InLobby y -> do liftIO $ writeIORef ref (InLobby x)
+                            setHeader "Access-Control-Allow-Origin" "*"
+                            text (T.pack $ "Change from process \""
+                                         ++ show y ++ "\" to process \""
+                                         ++ show x ++ "\"")
